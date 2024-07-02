e7fcaf58eceb3d9e9e5ca64ebd4a0c2641e99627
hala-s <hala_mallak6522@hotmail.com>
2024-07-03 00:59:25 +0300

    updates

--- js/app.js
+++ js/app.js
@@ -1,113 +1,82 @@
 /**
- * 
  * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
+ * This exercise programmatically builds the navigation menu,
+ * scrolls to anchors from the navigation, and highlights
+ * the section in the viewport upon scrolling.
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
-
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
+ */
 
 /**
  * Define Global Variables
- * 
-*/
-const sections = document.querySelectorAll('section');
+ */
+const sections = Array.from(document.querySelectorAll('section'));
 const navList = document.getElementById('navbar__list');
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-// function createNavItem(section){
-//     const navItem = document.createElement('li');
-//     const navLink = document.createElement('a');
-//     // use getAttribute to access data attribute
-//     navLink.textContent = section.getAttribute('data-nav');
-//     navLink.href = `#${section.id}`;
-//     navLink.classList.add('menu__link');
-//     navItem.appendChild(navLink);
-//     return navItem;
-// }
 
 /**
  * Check if a section is in the viewport
  * @param {HTMLElement} section 
  */
-const isInViewport = (section) => {
-    const rect = section.getBoundingClientRect();
-    return rect.top >= 0 && rect.top <= window.innerHeight / 2;
-}
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
+const sectionIsInView = section => {
+  const rect = section.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
 
 /**
  * Build the navigation menu
  */
-const buildNav = () => {
-    const fragment = document.createDocumentFragment();
-    sections.forEach((section) => {
-      const listItem = document.createElement('li');
-      const anchor = document.createElement('a');
-      anchor.classList.add('menu__link');
-      anchor.href = `#${section.id}`;
-      anchor.textContent = section.dataset.nav;
-      listItem.appendChild(anchor);
-      fragment.appendChild(listItem);
-    });
-    navList.appendChild(fragment);
-  };
-
-
-// Add class 'active' to section when near top of viewport
-const setActiveSection = () => {
-    sections.forEach((section) => {
-      if (isInViewport(section)) {
-        section.classList.add('your-active-class');
-      } else {
-        section.classList.remove('your-active-class');
-      }
-    });
-  };
-
-// Scroll to anchor ID using scrollTO event
-const scrollToSection = (event) => {
-    event.preventDefault();
-    if (event.target.nodeName === 'A') {
-      const targetSection = document.querySelector(event.target.getAttribute('href'));
-      targetSection.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
+const createNavMenu = () => {
+  const fragment = new DocumentFragment();
+  sections.forEach(section => {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `<a href="#${section.id}" class="menu__link">${section.dataset.nav}</a>`;
+    fragment.appendChild(listItem);
+  });
+  navList.appendChild(fragment);
+};
 
 /**
- * End Main Functions
- * Begin Events
- * 
-*/
+ * Add class 'active' to section when near top of viewport
+ */
+const highlightActiveSection = () => {
+  sections.forEach(section => {
+    if (sectionIsInView(section)) {
+      section.classList.add('your-active-class');
+    } else {
+      section.classList.remove('your-active-class');
+    }
+  });
+};
 
-// Build menu 
-document.addEventListener('DOMContentLoaded', buildNav);
+/**
+ * Scroll to anchor ID using scrollIntoView event
+ * @param {Event} event 
+ */
+const smoothScrollToSection = event => {
+  if (event.target.nodeName === 'A') {
+    event.preventDefault();
+    const targetSection = document.querySelector(event.target.getAttribute('href'));
+    targetSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+};
+
+/**
+ * Event listeners
+ */
+// Build menu when the DOM content is loaded
+document.addEventListener('DOMContentLoaded', createNavMenu);
 
 // Scroll to section on link click
-navList.addEventListener('click', scrollToSection);
+navList.addEventListener('click', smoothScrollToSection);
 
-// Set sections as active
-document.addEventListener('scroll', setActiveSection);
-
-
+// Set sections as active on scroll
+document.addEventListener('scroll', highlightActiveSection);
